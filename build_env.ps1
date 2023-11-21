656fd30a91e4360ecb45d9ed65830ffd1159fc8f
daniel.gorduz <daniel.gorduz@Daniel.Gorduz-MBP>
2023-11-21 23:42:25 +0200

    fix

--- build_env.ps1
+++ build_env.ps1
@@ -1,5 +1,28 @@
 $env:ASPNETCORE_URLS="http://*:8080"
-dotnet build "BlazorApp.csproj" -c Development
-dotnet publish "BlazorApp.csproj" -c Development
-start dotnet C:\Users\Administrator\blazor_app_demo\bin\Development\net8.0\BlazorApp.dll
-Start-Sleep -Seconds 5
+
+# Start the dotnet build process in the background
+$buildJob = Start-Job -ScriptBlock {
+    Start-Process "dotnet" -ArgumentList "build", "BlazorApp.csproj", "-c", "Development" -NoNewWindow
+}
+
+# Wait for the build process to complete and retrieve the job result
+$null = Receive-Job $buildJob
+
+# Start the dotnet publish process in the background
+$publishJob = Start-Job -ScriptBlock {
+    Start-Process "dotnet" -ArgumentList "publish", "BlazorApp.csproj", "-c", "Development" -NoNewWindow
+}
+
+# Wait for the publish process to complete and retrieve the job result
+$null = Receive-Job $publishJob
+
+# Start the dotnet BlazorApp.dll process in the background
+$appJob = Start-Job -ScriptBlock {
+    Start-Process "dotnet" -ArgumentList "C:\Users\Administrator\blazor_app_demo\bin\Development\net8.0\BlazorApp.dll" -NoNewWindow
+}
+
+# Save the job information to be able to stop the process later
+$jobs = @($buildJob, $publishJob, $appJob)
+$jobs | Format-Table -AutoSize | Out-File -FilePath "C:\Users\Administrator\blazor_app_demo\jobs.txt"
+
+# Optionally, you can close the PowerShell session, and the background jobs will continue
