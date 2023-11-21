584db6f04492d60736383259fc2c59d7b0ff8fb2
daniel.gorduz <daniel.gorduz@Daniel.Gorduz-MBP>
2023-11-21 15:36:01 +0200

    feat: added build.ps1

---
+++ build_env.ps1
@@ -0,0 +1,4 @@
+$env:ASPNETCORE_URLS="http://*:8080"
+dotnet build "BlazorApp.csproj" -c Development
+dotnet publish "BlazorApp.csproj" -c Development 
+dotnet C:\Users\Administrator\Desktop\blazor_app_demo\bin\Development\net8.0\BlazorApp.dll
