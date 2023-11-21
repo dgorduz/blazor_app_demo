9693cbd441beb4d4c5b2f0ef60bd3e8e237ea8ef
daniel.gorduz <daniel.gorduz@Daniel.Gorduz-MBP>
2023-11-21 22:31:07 +0200

    fix

--- build_env.ps1
+++ build_env.ps1
@@ -1,4 +1,4 @@
 $env:ASPNETCORE_URLS="http://*:8080"
 dotnet build "BlazorApp.csproj" -c Development
 dotnet publish "BlazorApp.csproj" -c Development
-dotnet C:\Users\Administrator\blazor_app_demo\bin\Development\net8.0\BlazorApp.dll
+start dotnet C:\Users\Administrator\blazor_app_demo\bin\Development\net8.0\BlazorApp.dll
