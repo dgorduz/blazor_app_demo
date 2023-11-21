4513d6f362e929e16e2ad1941b4c7237d19763df
daniel.gorduz <daniel.gorduz@Daniel.Gorduz-MBP>
2023-11-21 23:48:19 +0200

    fix

--- build_env.ps1
+++ build_env.ps1
@@ -1,24 +1,24 @@
 $env:ASPNETCORE_URLS="http://*:8080"
 
 # Start the dotnet build process in the background
-$buildJob = Start-Job -ScriptBlock {
-    Start-Process "dotnet" -ArgumentList "build", "BlazorApp.csproj", "-c", "Development" -NoNewWindow
+$buildJob = Start-Job -RunAs32 -ScriptBlock {
+    Start-Process "dotnet" -ArgumentList "build", "BlazorApp.csproj", "-c", "Development" -NoNewWindow -PassThru
 }
 
 # Wait for the build process to complete and retrieve the job result
 $null = Receive-Job $buildJob
 
 # Start the dotnet publish process in the background
-$publishJob = Start-Job -ScriptBlock {
-    Start-Process "dotnet" -ArgumentList "publish", "BlazorApp.csproj", "-c", "Development" -NoNewWindow
+$publishJob = Start-Job -RunAs32 -ScriptBlock {
+    Start-Process "dotnet" -ArgumentList "publish", "BlazorApp.csproj", "-c", "Development" -NoNewWindow -PassThru
 }
 
 # Wait for the publish process to complete and retrieve the job result
 $null = Receive-Job $publishJob
 
 # Start the dotnet BlazorApp.dll process in the background
-$appJob = Start-Job -ScriptBlock {
-    Start-Process "dotnet" -ArgumentList "C:\Users\Administrator\blazor_app_demo\bin\Development\net8.0\BlazorApp.dll" -NoNewWindow
+$appJob = Start-Job -RunAs32 -ScriptBlock {
+    Start-Process "dotnet" -ArgumentList "C:\Users\Administrator\blazor_app_demo\bin\Development\net8.0\BlazorApp.dll" -NoNewWindow -PassThru
 }
 
 # Save the job information to be able to stop the process later
