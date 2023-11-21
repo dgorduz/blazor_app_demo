6d6a10be3c4d3b2862f8a75cffcf171f5c5363d2
daniel.gorduz <daniel.gorduz@Daniel.Gorduz-MBP>
2023-11-21 22:38:07 +0200

    revert

--- build_env.ps1
+++ build_env.ps1
@@ -2,3 +2,4 @@ $env:ASPNETCORE_URLS="http://*:8080"
 dotnet build "BlazorApp.csproj" -c Development
 dotnet publish "BlazorApp.csproj" -c Development
 start dotnet C:\Users\Administrator\blazor_app_demo\bin\Development\net8.0\BlazorApp.dll
+Start-Sleep -Seconds 5
