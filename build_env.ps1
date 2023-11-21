881357a4eb97f0ca31363eed6f3212f28499359f
daniel.gorduz <daniel.gorduz@Daniel.Gorduz-MBP>
2023-11-21 21:56:56 +0200

    fix: run in background

--- build_env.ps1
+++ build_env.ps1
@@ -1,4 +1,6 @@
 $env:ASPNETCORE_URLS="http://*:8080"
 dotnet build "BlazorApp.csproj" -c Development
 dotnet publish "BlazorApp.csproj" -c Development
-Start-Process "dotnet" -ArgumentList "C:\Users\Administrator\blazor_app_demo\bin\Development\net8.0\BlazorApp.dll" -NoNewWindow 
+Start-Job -ScriptBlock {
+    Start-Process "dotnet" -ArgumentList "C:\Users\Administrator\blazor_app_demo\bin\Development\net8.0\BlazorApp.dll" -NoNewWindow
+}
